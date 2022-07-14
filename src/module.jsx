f55beba8fd93f61523fa5830e91d1813f4418e3b
Elena Kanash <hird250@gmail.com>
2022-07-14 18:58:57 +0300

    init project

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import stones from './img/1.jpg'
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img width={600} src={stones}/>
+  </>
+);
+
+export default Module;
